cca3f6c955cd4fb7045a4be197b39bcd25bdbfc8
sasaki-q <tsasaki@netprotections.co.jp>
2022-10-07 15:17:26 +0900

    Add: taskdef appspec

--- main.go
+++ main.go
@@ -8,13 +8,14 @@ import (
 
 type Response struct {
 	Message string
+	Env     string
 }
 
 func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, Response{Message: "success"})
+		c.JSON(200, Response{Message: "success", Env: os.Getenv("FROM_TASK")})
 	})
 
 	r.Run(":" + os.Getenv("PORTS"))
